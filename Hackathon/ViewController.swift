37954ce16fd97d430b0322a98b089001f64fb2e6
Julio César Guzmán <julio.guzman@me.com>
2016-01-01 16:40:09 -0600

    Initial Commit

---
+++ Hackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackathon
+//
+//  Created by Julio César Guzman on 1/1/16.
+//  Copyright © 2016 Julio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
