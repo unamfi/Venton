a5263c9b12178d264667b332facf151ae5c1f82c
Julio César Guzmán <julio.guzman@me.com>
2016-01-02 17:02:03 -0600

    Add hack comments

--- Hackathon/Ericsson.swift
+++ Hackathon/Ericsson.swift
@@ -9,5 +9,39 @@
 import Foundation
 import Alamofire
 
+//     KNOW THE VEHICLE
+
+//    - (void) checkRequestStatus:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) viewDiagnosticData:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) getVehicleStatus:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) getBusInfo:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+
+//     CONTROL THE VEHICLE
+
+//    - (void) doorUnlock:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) doorLock:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) engineOn:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) engineOff:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) honkAndBlink:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) getVehicleStatus:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+
+//     KNOW THE PASSENGER
+
+//    - (void) addAPassenger:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) updateAPassenger:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) deleteAPassenger:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) viewAPassenger:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) searchPassengers:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+
+//     KNOW THE PASSENGER
+
+//    - (void) consume:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) getProducts:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) getUserPurchases:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) purchase:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+//    - (void) refund:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
+
+
 class EricssonManager {
+    
 }
