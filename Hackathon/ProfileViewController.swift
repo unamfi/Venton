9f7fe88292d6897400f20b8a0b92f7b41310ac46
Jonathan Velazquez <jonhysiker@icloud.com>
2016-01-03 13:21:28 -0600

    asdfghjkl

--- Hackathon/ProfileViewController.swift
+++ Hackathon/ProfileViewController.swift
@@ -9,18 +9,139 @@
 import Foundation
 import UIKit
 
+
+
+class tableDataSource: NSObject,UITableViewDataSource {
+    var descriptionTexts = [String]()
+    var valuesTexts = [String]()
+    var valueColors = [UIColor]()
+    var tableView:UITableView!
+    
+    
+    init(descriptionTexts:[String],valuesTexts:[String],valueColors:[UIColor],tableView:UITableView) {
+        self.descriptionTexts = descriptionTexts
+        self.valuesTexts = valuesTexts
+        self.valueColors = valueColors
+        self.tableView = tableView
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return descriptionTexts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ProfileCell") as! ProfileCell
+        cell.setCell(descriptionTexts[indexPath.row], value: valuesTexts[indexPath.row], valueColor: valueColors[indexPath.row])
+        return cell
+    }
+}
+
+
+
+
 class ProfileViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var stateTableView: UITableView!
+    
+    var ericssonManager = EricssonManager()
+    var isLogged:Bool = false
+    var descriptionState = [String]()
+    var valueState = [String]()
+    var valueColor = [UIColor]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        login()
+        configTable()
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        getDiagnosticInfo()
+    }
+    
+    func login(){
+        ericssonManager.logIn {
+            (success) -> () in
+            if success {
+               self.isLogged = true
+                    
+            }else{
+                self.isLogged = false
+            }
+        }
+
+    }
+    
+    
+    func configTable(){
+        stateTableView.dataSource = tableDataSource(descriptionTexts: descriptionState, valuesTexts: valueState, valueColors: valueColor, tableView: stateTableView)
+    }
+    
+    
+    
+    
+    func getDiagnosticInfo(){
+            ericssonManager.getDiagnosticDataStatus{
+                (info)->() in
+                var statusReport = info["statusReport"] as! NSDictionary
+                for (key,value) in statusReport {
+                    
+                    switch key as! String {
+                        case "abs":
+                            self.descriptionState.append("ABS")
+                            
+                            if value as! String == "OK"{
+                                self.valueState.append("Looks Good")
+                                self.valueColor.append(UIColor.greenColor())
+                            }else{
+                                self.valueState.append("Danger")
+                                self.valueColor.append(UIColor.redColor())
+                            }
+                            break;
+                        case "airbags":
+                            self.descriptionState.append("AirBags")
+                            if value as! String == "OK"{
+                                self.valueState.append("Looks Good")
+                                self.valueColor.append(UIColor.greenColor())
+                            }else{
+                                self.valueState.append("Danger")
+                                self.valueColor.append(UIColor.redColor())
+                            }
+                        break
+                    case "transmission":
+                        self.descriptionState.append("Transmission")
+                        if value as! String == "OK"{
+                            self.valueState.append("Looks Good")
+                            self.valueColor.append(UIColor.greenColor())
+                        }else{
+                            self.valueState.append("Danger")
+                            self.valueColor.append(UIColor.redColor())
+                        }
+                        break
+                    default:
+                        break
+                    }
+                }
+                self.stateTableView.reloadData()
+
+                
+            }
+        
+    }
+    
+    
     
     
 }
--- Hackathon/SettingsViewController.swift
+++ Hackathon/SettingsViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Mapbox
 
-class SettingsViewController: UIViewController {
+
+
+
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var profileButton: UIButton!
     @IBOutlet weak var searchRidesTextField: UITextField!
@@ -19,24 +23,168 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var rideSearchModePicker: UIPickerView!
     
     
+    @IBOutlet weak var mapContainerView: UIView!
+    
+    
+    
+    var ridersViews = [UIView]()
+    var waitTimeViews = [UIView]()
+    var ridePriceViews = [UIView]()
+    var rideSearchModeViews = [UIView]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupView()
+        
+        
+        setupMap()
+        getPickerViews()
+        setupPickers()
+        configView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-    func setupView(){
-        
-        profileButton.layer.cornerRadius = 20
-        profileButton.clipsToBounds = true
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        searchRidesTextField.resignFirstResponder()
+        return true
     }
+    
+    
+    func configView(){
+        searchRidesTextField.delegate = self
+        let leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 0))
+        searchRidesTextField.leftView = leftView
+        searchRidesTextField.leftViewMode = .Always
+    }
+    
+    
+    func setupMap(){
+       // let styleURL = NSURL(string: "mapbox://styles/jonhysiker.a85a88cd")
+        let mapBoxView = MGLMapView(frame: self.mapContainerView.frame ,styleURL: MGLStyle.darkStyleURL())
+        mapBoxView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        mapBoxView.setCenterCoordinate(CLLocationCoordinate2D(latitude: 20.6530501,
+            longitude: -103.3913392),
+            zoomLevel: 13, animated: false)
+        mapContainerView.addSubview(mapBoxView)
+    }
+    
+    
+    func getPickerViews(){
+        ridersViews = generateViews(["1","2","3","4"], imageName: "ridesCount")
+        waitTimeViews = generateViews(["5 min","10 min"], imageName: "waitTime")
+        ridePriceViews = generateViews(["$20","$50","$100"], imageName: "ridePrice")
+        rideSearchModeViews = generateViews(["Coche 1"], imageName: "rideSearchMode")
+    }
+    
+    
+    func setupPickers(){
+        
+        ridersCountPicker.dataSource = self
+        ridersCountPicker.delegate = self
+        
+        waitTimePicker.dataSource = self
+        waitTimePicker.delegate = self
+        
+        ridePricePicker.dataSource = self
+        ridePricePicker.delegate = self
+        
+        rideSearchModePicker.dataSource = self
+        rideSearchModePicker.delegate = self
+        
+        
+    }
+    
+    func generateViews(titles:[String],imageName:String)->[UIView]{
+        var componentsViews = [UIView]()
+        let firstComponent = UIImageView(image: UIImage(named: imageName))
+        firstComponent.frame = CGRect(x: 0, y: 0, width: 30, height: 20)
+        firstComponent.contentMode = .ScaleAspectFit
+        firstComponent.backgroundColor = UIColor.clearColor()
+        componentsViews.append(firstComponent)
+        for text in titles {
+            let labelComponent = UILabel(frame: CGRect(x: 5, y: 5, width: 70, height: 20))
+            labelComponent.text = text
+            labelComponent.textColor = UIColor.whiteColor()
+            labelComponent.backgroundColor = UIColor.clearColor()
+            componentsViews.append(labelComponent)
+        }
+        return componentsViews
+    }
+    
+    
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView {
+        case ridersCountPicker:
+            return ridersViews.count
+        case waitTimePicker:
+            return waitTimeViews.count
+        case ridePricePicker:
+            return ridePriceViews.count
+        case rideSearchModePicker:
+            return rideSearchModeViews.count
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
+        var vista = UIView(frame: CGRect(x: 0, y: 0, width: 60, height: 30))
+        vista.backgroundColor = UIColor.blackColor()
+        switch pickerView {
+        case ridersCountPicker:
+            vista.addSubview(ridersViews[row])
+            return vista
+        case waitTimePicker:
+            vista.addSubview(waitTimeViews[row])
+            return vista
+        case ridePricePicker:
+            
+            return ridePriceViews[row]
+        case rideSearchModePicker:
+            return rideSearchModeViews[row]
+        default:
+            return rideSearchModeViews[row]
+        }
+    }
+    
+    /*func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        switch pickerView {
+        case ridersCountPicker:
+            return "rides"
+        case waitTimePicker:
+            return "wait"
+        case ridePricePicker:
+            return "ride"
+        case rideSearchModePicker:
+            return "rideSearch"
+        default:
+            return ""
+        }
+    }*/
+    
+    
+    func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        
+        return 40
+
+    }
+    
+    
+    
+    
 
 }
 
