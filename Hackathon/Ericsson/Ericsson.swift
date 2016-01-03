1ac18a3e0a6ca6ffa1f37cbed94f63b1bb29cc3c
Julio César Guzmán <julio.guzman@me.com>
2016-01-03 05:35:55 -0600

    Remove unnecessary comments

--- Hackathon/Ericsson/Ericsson.swift
+++ Hackathon/Ericsson/Ericsson.swift
@@ -9,43 +9,6 @@
 import Foundation
 import Alamofire
 
-//    - (void) login:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-
-//     KNOW THE VEHICLE
-
-//    - (void) checkRequestStatus:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) viewDiagnosticData:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) getVehicleStatus:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) getBusInfo:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-
-//     CONTROL THE VEHICLE
-
-//    - (void) doorUnlock:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) doorLock:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) engineOn:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) engineOff:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) honkAndBlink:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) getVehicleStatus:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-
-//     KNOW THE PASSENGER
-
-//    - (void) addAPassenger:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) updateAPassenger:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) deleteAPassenger:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) viewAPassenger:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) searchPassengers:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-
-//     KNOW THE PASSENGER
-
-//    - (void) consume:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) getProducts:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) getUserPurchases:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) purchase:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-//    - (void) refund:(NSDictionary *)params completion:(ASDPRequestCompletionBlock)completion;
-
-// Old params
-//    var params : [String: AnyObject] = ["longitude": 55.578545, "latitude": 14.254875, "accuracy": 6, "route" : ["vin" : "3115361391" ]]
-
 typealias BooleanClosure = (Bool) -> ()
 typealias SuccessClosure = BooleanClosure
 private typealias HandleRequestID = (Int) -> ()
