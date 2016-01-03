7815434c9405e095dad74df4c72f9f87b2af5da9
Jonathan Velazquez <jonhysiker@icloud.com>
2016-01-03 02:59:17 -0600

    Add Refine Search items for menu

---
+++ Hackathon/RefineSearchItemView.swift
@@ -0,0 +1,85 @@
+//
+//  RefineSearchView.swift
+//  Hackathon
+//
+//  Created by Jonathan Velazquez on 02/01/16.
+//  Copyright © 2016 Julio. All rights reserved.
+//
+
+import UIKit
+
+class PickerDataSource: NSObject,UIPickerViewDataSource {
+    var countData:Int = 0
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return countData
+    }
+    
+    func setData(countData: Int){
+        self.countData = countData
+    }
+    
+}
+
+class PickerDelegate: NSObject,UIPickerViewDelegate {
+    
+    var pickerData = [UIView]()
+    
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
+        return pickerData[component]
+    }
+    
+    func setData(pickerData: [UIView]){
+        self.pickerData = pickerData
+    }
+
+}
+
+
+
+class RefineSearchView: UIViewController {
+    
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData = [UIView]()
+    var pickerDelegate = PickerDelegate()
+    var pickerDataSource = PickerDataSource()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupPicker()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    func setupPicker(){
+        picker.delegate = pickerDelegate
+        picker.dataSource = pickerDataSource
+    }
+    
+    
+    func setData(pickerData:[UIView]){
+        self.pickerData = pickerData
+        pickerDelegate.setData(pickerData)
+        pickerDataSource.setData(pickerData.count)
+        
+        picker.reloadAllComponents()
+        
+    }
+
+}
+
+
+
+
+
--- Hackathon/RefineSearchView.swift
+++ Hackathon/RefineSearchView.swift
@@ -2,88 +2,34 @@
 //  RefineSearchView.swift
 //  Hackathon
 //
-//  Created by Jonathan Velazquez on 02/01/16.
+//  Created by Jonathan Velazquez on 03/01/16.
 //  Copyright © 2016 Julio. All rights reserved.
 //
 
 import UIKit
 
-class PickerDataSource: NSObject,UIPickerViewDataSource {
-    var countData:Int = 0
-    
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 1
-    }
-    
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return countData
-    }
-    
-    func setData(countData: Int){
-        self.countData = countData
-    }
-    
-}
-
-class PickerDelegate: NSObject,UIPickerViewDelegate {
-    
-    var pickerData = [UIView]()
-    
-    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
-        return pickerData[component]
-    }
-    
-    func setData(pickerData: [UIView]){
-        self.pickerData = pickerData
-    }
-
-}
-
-
-
 class RefineSearchView: UIViewController {
-    
-    
-    @IBOutlet weak var picker: UIPickerView!
-    
-    var pickerData = [UIView]()
-    var pickerDelegate = PickerDelegate()
-    var pickerDataSource = PickerDataSource()
-    
-    
-   
-
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
+        // Dispose of any resources that can be recreated.
     }
     
-    func setupPicker(){
-        picker.delegate = pickerDelegate
-        picker.dataSource = pickerDataSource
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    
-    
-    func setData(pickerData:[UIView]){
-        self.pickerData = pickerData
-        pickerDelegate.setData(pickerData)
-        pickerDataSource.setData(pickerData.count)
-        
-        picker.reloadAllComponents()
-        
-    }
+    */
 
 }
-
-
-
-
-
