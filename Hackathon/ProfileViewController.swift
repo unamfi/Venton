c620efa8f93fcd090b1db3088a5ad1d4591125a3
Julio César Guzmán <julio.guzman@me.com>
2016-01-03 08:33:15 -0600

    Add animation and stuff

---
+++ Hackathon/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  Hackathon
+//
+//  Created by Julio César Guzman on 1/3/16.
+//  Copyright © 2016 Julio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
