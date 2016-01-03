88bd4019ccccefcd1223581c517b33d2749c7d09
Julio César Guzmán <julio.guzman@me.com>
2016-01-03 08:53:06 -0600

    Add CircleButton and the most guapo asset ever :D

---
+++ Hackathon/CircleView.swift
@@ -0,0 +1,36 @@
+//
+//  CircleView.swift
+//  Hackathon
+//
+//  Created by Julio César Guzman on 1/3/16.
+//  Copyright © 2016 Julio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Spring
+
+@IBDesignable
+class CircleButton: SpringButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        initialize()
+    }
+    
+    func initialize() {
+        self.layer.cornerRadius = 0.5 * self.bounds.size.width
+        self.clipsToBounds = true
+    }
+    
+}
